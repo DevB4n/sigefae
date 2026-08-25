0a5f0d31b1bf10801de9a1fc2f515968eb7e26d2
DevB4n <buenochaconesteban@gmail.com>
2026-08-25 08:28:19 -0500

    feat: :bug: path

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -16,7 +16,7 @@ func main() {
 	// Configuración
 	// ========================================
 
-	cfg, err := env.Load(".env")
+	cfg, err := env.Load("../../../../../env_hp")
 	if err != nil {
 		log.Fatal(err)
 	}
